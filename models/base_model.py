1ba45fb550bebe449b577d6b778eef14227ae275
conkobar <connorkleine@gmail.com>
2022-10-26 19:42:56 -0500

    finished task 6 as outlined, i believe

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,31 @@
 import uuid
 from datetime import datetime
 from models import storage
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 
 
 Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(
+        String(60),
+        nullable=False,
+        primary_key=True
+    )
+    created_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow()
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow()
+    )
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -31,6 +49,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +60,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            dictionary.pop('_sa_instance_state', None)
         return dictionary
+
+    def delete(self):
+        """deletes current instance from storage"""
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+import os
+import models
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship(
+            'City',
+            backref='state',
+            cascade='all, delete, delete-orphan'
+        )
+    else:
+        @property
+        def cities(self):
+            """returns list of city instances"""
+            towns = []
+            for town in models.storage.all(City).values():
+                if town.state_id == self.id:
+                    towns.append(town)
+            return towns
