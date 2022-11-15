330474258832f73d0493c15a072fb427c2e6774b
conkobar <connorkleine@gmail.com>
2022-11-15 09:54:52 -0600

    fixing pep8 and keyword

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,13 @@
 """
 from flask import Flask
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, strict_slashes=False)
+    app.run(host="0.0.0.0", port=5000)
