55999c9fbe8d09a63c41afec065eb5c6d3ec3448
conkobar <connorkleine@gmail.com>
2022-11-15 11:06:24 -0600

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def cisfun(text):
+    return "C {}".format(
+        text.replace("_", " ")
+    )
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def simpleisbest(text="is cool"):
+    return "Python {}".format(
+        text.replace("_", " ")
+    )
+
+
+@app.route('/number/<int:n>')
+def numbz(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def numbzy_temp(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
