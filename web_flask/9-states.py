8337843c3870fb6461656634911bc0555926af15
conkobar <connorkleine@gmail.com>
2022-11-17 14:29:05 -0600

    idk

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/states')
 @app.route('/states_list')
 def stately():
     return render_template("7-states_list.html", db=storage.all(State))
@@ -19,6 +20,11 @@ def stately():
 @app.route('/cities_by_states')
 def cities_by_states():
     return render_template("8-cities_by_states.html", db=storage.all(State))
+
+
+@app.route('/states/<id>')
+def states_route(id):
+    return render_template("9-states.html", db=storage.all(State), id=id)
 
 
 @app.teardown_appcontext
